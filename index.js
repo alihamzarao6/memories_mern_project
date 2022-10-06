3d3a2d8fc1e0ae1c846799ad908c7e85a7b5fb92
Ali Hamza Rao <104719764+alihamzarao6@users.noreply.github.com>
2022-10-05 23:06:54 -0700

    make it better 3

--- index.js
+++ index.js
@@ -15,20 +15,20 @@ app.use(cors());
 
 app.use("/posts", postRoutes); // it means every path in postRoutes will start with /posts/
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
   res.send("Hello to Memories Api");
-})
+});
 
-const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || "0.0.0.0";
 
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() =>
-    app.listen(PORT, () => {
+    app.listen(PORT, HOST, () => {
       console.log(`server is running on port no. ${PORT}`);
     })
   )
